8be505166a4f2a82153b440d1526399de830a713
felipeDev303 <felipe.morales.roa@ciisa.cl>
2024-11-19 02:08:02 -0300

    more code to print.py

--- print.py
+++ print.py
@@ -7,4 +7,36 @@ print("  *   *")
 print("  *   *")
 print("  *****")
 
+# Minimizar el número de invocaciones de la función print() insertando la secuencia \n en las cadenas.
 print("    *   \n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *   *\n  *****")
+
+# Hacer la flecha dos veces más grande (pero mantener las proporciones).
+print("          *")
+print("         * *")
+print("        *   *")
+print("       *     *")
+print("      *       *")
+print("     *         *")
+print("    *           *")
+print("   *             *")
+print("  *               *")
+print(" *                 *")
+print("*******       *******")
+print("      *       *")
+print("      *       *")
+print("      *       *")
+print("      *       *")
+print("      *********")
+
+# Duplicar la flecha, colocando ambas flechas lado a lado; nota: una cadena se puede
+# multiplicar usando el siguiente truco: "cadena" * 2 producirá "cadenacadena"
+# (te contaremos más sobre ello pronto).
+print("    *    "*2)
+print("   * *   "*2)
+print("  *   *  "*2)
+print(" *     * "*2)
+print("***   ***"*2)
+print("  *   *  "*2)
+print("  *   *  "*2)
+print("  *****  "*2)
+
