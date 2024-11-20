acea7c6faaee064461d38fda80b56d2190313c2c
felipeDev303 <felipe.morales.roa@ciisa.cl>
2024-11-20 02:40:59 -0300

    add string

--- string.py
+++ string.py
@@ -2,3 +2,7 @@
 print('I\'m Monty Python')
 
 print("I'm Monty Python")
+
+# Escribe una sola línea de código, utilizando la función print(), así como los caracteres de
+# nueva línea y escape, para obtener la salida esperada de tres líneas.
+print('"Estoy"\n""aprendiendo""\n"""Python"""')
